763c51d18f4c7dcd723e34a4199dd58e103effb1
woodman <woodman@edetw.com>
2024-09-05 15:55:02 +0800

    加上夾附件說明

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -28,14 +29,20 @@ namespace ConsoleApp1
             wkf.Url = "http://localhost/UOF_CAR/PublicAPI/WKF/Wkf.asmx";
 
             string result = "";
-           // result = wkf.GetFormList(token);
+            // result = wkf.GetFormList(token);
 
-         //   result=wkf.GetFormStructure(token, "6fd8fa9c-ecc2-41b3-8315-7c77d9e9e676");
+            //   result=wkf.GetFormStructure(token, "6fd8fa9c-ecc2-41b3-8315-7c77d9e9e676");
 
-           // Console.WriteLine(result);
+            // Console.WriteLine(result);
+
+            //抓取附件 先複製到C:\UOF_Temp\WKF_TempFile
+            FileInfo fileInfo = new FileInfo("D:\\採購資訊.pdf");
+            fileInfo.CopyTo("C:\\UOF_Temp\\WKF_TempFile\\" + fileInfo.Name, true);
+
 
             XElement formXE = new XElement("Form");
-            formXE.Add(new XAttribute("formVersionId", "6fd8fa9c-ecc2-41b3-8315-7c77d9e9e676"));
+            formXE.Add(new XAttribute("formVersionId", 
+                "6fd8fa9c-ecc2-41b3-8315-7c77d9e9e676"));
             formXE.Add(new XAttribute("urgentLevel", "2"));
 
             XElement applicantXE = new XElement("Applicant");
@@ -43,6 +50,17 @@ namespace ConsoleApp1
             applicantXE.Add(new XAttribute("groupId", ""));
             applicantXE.Add(new XAttribute("jobTitleId", ""));
 
+            XElement attachXE= new XElement("Attach");
+            attachXE.Add(new XAttribute("IsNeedTransfer", "true"));
+            attachXE.Add(new XAttribute("IsDeleteTemp", "true"));
+
+            XElement attachItemXE = new XElement("AttachItem");
+            attachItemXE.Add(new XAttribute("filePath", "採購資訊.pdf"));
+            attachXE.Add(attachItemXE);
+            applicantXE.Add(attachXE);
+            //          <Attach IsNeedTransfer="true" IsDeleteTemp="false">
+            //  <AttachItem filePath="採購資訊.pdf" />
+            //</Attach>
             XElement commentXE = new XElement("Comment", "");
 
             XElement formFieldValueXE = new XElement("FormFieldValue");
@@ -73,6 +91,7 @@ namespace ConsoleApp1
 
             //  Console.WriteLine(formXE.ToString());
             result = wkf.SendForm(token, formXE.ToString());
+            Console.WriteLine(formXE.ToString());
             Console.WriteLine(result);
 
             //            <Form urgentLevel="2" formVersionId="6fd8fa9c-ecc2-41b3-8315-7c77d9e9e676">
